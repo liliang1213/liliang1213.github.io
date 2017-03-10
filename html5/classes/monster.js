07b574856eabf85bef826928cc6765f3fbb5efe5
liliang1213 <dreamer_534686577@126.com>
2017-03-10 22:40:29 +0800

    second blood

--- html5/classes/monster.js
+++ html5/classes/monster.js
@@ -2,6 +2,8 @@ function Monster (img) {
     this.x = 0;
     this.y = 0;
     this.img = img;
+    this.width=this.img.width;
+    this.height=this.img.height;
     this.rotation = 0;
     this.scaleX = 1;
     this.scaleY = 1;
@@ -13,6 +15,6 @@ Monster.prototype.draw = function (context) {
     context.rotate(this.rotation);
     context.scale(this.scaleX, this.scaleY);
     context.translate(-this.x,-this.y);//将画布原点移动
-    context.drawImage(this.img,this.x,this.y);
+    context.drawImage(this.img,this.x-this.width/2,this.y-this.height/2);
     context.restore();
 };

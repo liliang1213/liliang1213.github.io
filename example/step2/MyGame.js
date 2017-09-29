655b6c7b9d079428a20254e8eca503dcf37b9533
liliang1213 <dreamer_534686577@126.com>
2017-09-30 00:06:37 +0800

    step 3

--- example/step2/MyGame.js
+++ example/step2/MyGame.js
@@ -3,6 +3,7 @@
 function MyGame() {
     var width = gEngine.Core.mWidth;
     var height = gEngine.Core.mHeight;
+    var r1 = new Rectangle(new Vec2(width / 2, height / 2), 3, 3, 0);
 
     var up = new Rectangle(new Vec2(width / 2, 0), width, 3, 0);
     var down = new Rectangle(new Vec2(width / 2, height), width, 3, 0);
